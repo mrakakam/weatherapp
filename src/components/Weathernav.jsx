12e84f33e1a9cba7059612a87bf6f19e234e62c4
mrakakam <johnakakamjohn@gmail.com>
2025-09-18 17:11:39 -0700

    any commit message

--- src/components/Weathernav.jsx
+++ src/components/Weathernav.jsx
@@ -1,28 +1,49 @@
+import { useState, useEffect, useRef } from "react";
 
 import weatherlogo from '../resources/images/logo.svg'
 import settingsimg from '../resources/images/icon-units.svg'
 import dropdownimg from '../resources/images/icon-dropdown.svg'
 
 export const Weathernav = () => {
+ 
+  const [showMenu, setShowMenu] = useState(false);
+
+  const menuRef = useRef(null);
+
+  
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setShowMenu(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+ 
+ 
   return (
     <>
         <header>
 
               <nav className='flex flex-row  items-center justify-center gap-[50px]'>
 
-                  <img src={weatherlogo} alt="" />
+                  <img src={weatherlogo} alt="" id='imgid' />
 
-                  <div className='bg-[var(--Neutral-600)] px-[30px] py-[15px] flex flex-row items-center justify-center gap-[10px]  rounded-[20px]'>
+                  <div className='bg-[var(--Neutral-600)] px-[30px] py-[15px] flex flex-row items-center justify-center gap-[10px]  rounded-[20px]'  ref={menuRef} onClick={() => setShowMenu(prev => !prev)}>
 
                       <img src={settingsimg} alt="" />
 
                       <h5 className='text-[var(--Neutral-0)] text-[25px] font-bold '>Units</h5>
 
-                      <img src={dropdownimg} alt="" />
+                      <img src={dropdownimg} alt=""  />
 
                       {/* focus state for nav bar or unit */}
 
-                      <div className='hidden'>
+                      {showMenu && (
+
+                      <div >
 
                         <div>
                           
@@ -57,6 +78,9 @@ export const Weathernav = () => {
 
 
                       </div>
+
+                      )}
+
                       
                   </div>
 
